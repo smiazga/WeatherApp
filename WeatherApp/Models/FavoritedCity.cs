a26669d0e4ee836c370e78d1ab619c1895c835b6
Steve Miazga <steveo847@hotmail.com>
2025-06-29 18:41:15 -0500

    modify layout and nav for weather app, call alert then check weath api

---
+++ WeatherApp/Models/FavoritedCity.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp.Models
+{
+    public class FavoritedCity
+    {
+        public int Id { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ WeatherApp/wwwroot/scripts/custom.js
@@ -0,0 +1,23 @@
+﻿//window.checkWeather = () => {
+//    let weatherCityName = document.getElementById("weatherCity").value;
+//    alert(weatherCityName)
+//}
+
+window.checkWeather = (cityName) => {
+    //alert(cityName)
+    const API_KEY = "64f60853740a1ee3ba20d0fb595c97d5"
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                console.error(`HTTP error: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        })
+}

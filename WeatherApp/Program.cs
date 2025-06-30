4ebcde33a6f3d42bf0c3624bc03265fff19f8448
Steve Miazga <steveo847@hotmail.com>
2025-06-29 20:59:49 -0500

    error handling, error boundary on blazor component, set up logging with serilog

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,10 +1,23 @@
+using Serilog;
 using WeatherApp.Components;
+using WeatherApp.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Add global exception handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+//Add logging configuration
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File("Logs/mylogs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -14,6 +27,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
